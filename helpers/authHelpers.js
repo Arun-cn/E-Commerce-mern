8db6bcccd98de58ccf1193cbe5ed32ca10255603
Arun Chandran <53257113+arunchandran409@users.noreply.github.com>
2023-05-03 20:41:56 +0530

    add authhelper

---
+++ helpers/authHelpers.js
@@ -0,0 +1,15 @@
+import bcrypt from ('bcrypt')
+
+const haspassword = async (password)=>{
+    try {
+        const saltRound = 10;
+        const hasedPassword = await bcrypt.hash(password ,saltRound);
+        return hasedPassword;
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+const comparePassword = async (password,hasedPassword)=>{
+ return bcrypt.compare(password,hasedPassword);
+}
