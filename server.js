224712f8fb43c2b13087223414759865db948b24
Arun Chandran <53257113+arunchandran409@users.noreply.github.com>
2023-06-15 22:40:02 +0530

    fix __dirname is not defined

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import categoryRoutes from './routes/categoryRoutes.js';
 import productRouttes from './routes/productRoutes.js';
 import cors from'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 
 dotenv.config();
@@ -22,6 +23,11 @@ app.use(cors());
 
 app.use(express.json());
 app.use(morgan('dev'));
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 const buildPath = path.join(__dirname, 'client', 'build');
 app.use(express.static(buildPath));
 
