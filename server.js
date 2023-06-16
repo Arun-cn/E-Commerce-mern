a5060198cd43c1bd4b461d1f2e51ec0d149d24ec
Arun Chandran <53257113+arunchandran409@users.noreply.github.com>
2023-06-16 12:12:21 +0530

    fix loading issue

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const __dirname = path.dirname(__filename);
 const buildPath = path.join(__dirname, 'client', 'build');
 app.use(express.static(buildPath));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
   });
 //routers
