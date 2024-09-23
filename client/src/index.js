f719ae73deedc7bcc1f94a281452e7fefd29a634
Arun-cn <arunchandran9876@gmail.com>
2024-09-23 21:09:37 +0530

    feat(style): add sass in to project

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from'react-router-dom';
-import { AuthProvider } from './Context/auth';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/main.scss";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./Context/auth";
 import "antd/dist/reset.css";
 import { SearchProvider } from "./Context/search";
 import { CartProvider } from "./Context/cart";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-   <AuthProvider>
-       <SearchProvider>
-         <CartProvider>
-            <BrowserRouter>
-            <App />
-            </BrowserRouter>
-         </CartProvider>
-      </SearchProvider>
-   </AuthProvider>
+  <AuthProvider>
+    <SearchProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CartProvider>
+    </SearchProvider>
+  </AuthProvider>
 );
-
-
-
