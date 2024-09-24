d21a5b71eea47d1c760b1c2d9b571bafb1340789
Arun-cn <arunchandran9876@gmail.com>
2024-09-24 11:06:09 +0530

    refactor(client/nav): update navbar to new style

--- client/src/components/Layout/Headers.js
+++ client/src/components/Layout/Headers.js
@@ -1,102 +1,99 @@
-import React from 'react'
-import { NavLink ,Link } from 'react-router-dom'
-import { useAuth } from '../../Context/auth'
-import SearchInput from '../Form/SearchInput';
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { useAuth } from "../../Context/auth";
+import SearchInput from "../Form/SearchInput";
 import useCategory from "../../hooks/useCategory";
 import { Badge } from "antd";
 import { useCart } from "../../Context/cart";
 
-
 const Headers = () => {
-  const [auth,setAuth] = useAuth();
+  const [auth, setAuth] = useAuth();
   const [cart] = useCart();
   const categories = useCategory();
-  const handleLogout = ()=>{
-      setAuth({
-        ...auth,
-        user:null,
-        token:""
-
-      });
-      localStorage.removeItem("auth");
-
-  }
+  const handleLogout = () => {
+    setAuth({
+      ...auth,
+      user: null,
+      token: "",
+    });
+    localStorage.removeItem("auth");
+  };
   return (
-    <>
-   <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <Link className="navbar-brand" >Ecommerce APP</Link>
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <SearchInput />
-        <li className="nav-item">
-          <NavLink to="/" className="nav-link"   >Home</NavLink>
-        </li>
-        <li className="nav-item dropdown">
-                <Link
-                  className="nav-link dropdown-toggle"
-                  to={"/categories"}
-                  data-bs-toggle="dropdown"
-                >
-                  Categories
-                </Link>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" to={"/categories"}>
-                      All Categories
-                    </Link>
-                  </li>
-                  {categories?.map((c) => (
-                    <li>
-                      <Link
-                        className="dropdown-item"
-                        to={`/category/${c.slug}`}
-                      >
-                        {c.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo03"
+          aria-controls="navbarTogglerDemo03"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#">
+          <img
+            src="/logo/elite emporium logo-light.png"
+            alt="Logo"
+            width="200"
+            height="50"
+            className="d-inline-block align-top"
+          />
+        </a>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <div className="container-fluid  justify-content-center">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
               </li>
-        {!auth.user ? (<><li className="nav-item">
-          <NavLink to="/register" className="nav-link" >Register</NavLink>
-        </li>
-        <li className="nav-item">
-        <NavLink to = "/login" className="nav-link" >Login</NavLink>
-      </li>
-        </>
-        ) : (
-          <li className="nav-item dropdown">
-            <NavLink className="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              {auth?.user?.name}
-            </NavLink>
-            <ul className="dropdown-menu">
-              <li><NavLink to={`/dashboard/${auth?.user?.role === 1 ? "admin" : "user"}`} className="dropdown-item" >Dashboard</NavLink></li>
-              <li className="nav-item">
-                <NavLink onClick={handleLogout} to = "/login" className="dropdown-item" >Logout</NavLink>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  TREND NOW
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Fashion
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  New Arrivals
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Best Sellers
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Electronics
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Blog
+                </a>
               </li>
             </ul>
-          </li>
+          </div>
+          <SearchInput class="m-2" />
+          <div class="d-flex ms-2">
+            <button class="btn btn-outline-success me-1" type="submit ">
+              Login
+            </button>
+            <button class="btn btn-outline-success" type="submit ">
+              Register
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-        )}
-        <li className="nav-item">
-          <NavLink to="/cart" className="nav-link">
-              <Badge count={cart?.length} showZero offset={[10, -5]}>
-                Cart
-              </Badge>
-           </NavLink>
-        </li>
-      </ul>
-      
-    </div>
-  </div>
-</nav>
-
-     
-    </> )
-}
-
-export default Headers
+export default Headers;
