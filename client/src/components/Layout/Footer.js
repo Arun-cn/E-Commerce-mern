8363c8b9f5f743c8642ae78139ae8f56a01e744d
Arun-cn <arunchandran9876@gmail.com>
2024-12-13 20:07:52 +0530

    feat: add footer items and links

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,19 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='bg-dark text-light p3'>
-        <h1 className='text-center'>ALL Rights Reserved</h1>
-       <p className='text-center mt-3'>
-        <Link to ="/about">About</Link>
-        <Link to ="/contact">Contact</Link>
-        <Link to ="/policy">privacy policy</Link>
+    <footer className="footer mt-auto py-4 bg-dark text-light">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 mb-3">
+            <h5>About Us</h5>
+            <p>
+              We are dedicated to delivering the best products and services to
+              our customers. Stay connected with us!
+            </p>
+          </div>
+          <div className="col-md-4 mb-3">
+            <h5>Quick Links</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#home" className="footer-link">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#services" className="footer-link">
+                  Services
+                </a>
+              </li>
+              <li>
+                <a href="#contact" className="footer-link">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-4 mb-3">
+            <h5>Contact Us</h5>
+            <ul className="list-unstyled">
+              <li>Email: info@example.com</li>
+              <li>Phone: +123 456 7890</li>
+              <li>Location: City, Country</li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <div className="text-center">
+          <p>&copy; 2024 Your Company. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-       </p>
-         
-    </div>
-  )
-}
-
-export default Footer
+export default Footer;
